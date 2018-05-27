e67840ab491ebbc4b11faa0b5316ff89cafcfef8
DamjanJ <damjanjelic@gmail.com>
2018-05-27 16:42:02 +0200

    Initial commit

---
+++ app/src/main/java/org/zerocode/movienight/MainActivity.java
@@ -0,0 +1,20 @@
+package org.zerocode.movienight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Damjan on 26-May-18.
+ */
+
+public class MainActivity extends AppCompatActivity{
+
+    public Button Dugme1;
+    public TextView Tekst1;
+
+
+
+
+
+}
