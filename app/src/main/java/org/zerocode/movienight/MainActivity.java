cf3244318f96ae94f3b3055e65ba73611dbb7fed
DamjanJ <damjanjelic@gmail.com>
2018-05-27 21:02:03 +0200

    Creating Basics - Damjan

--- app/src/main/java/org/zerocode/movienight/MainActivity.java
+++ app/src/main/java/org/zerocode/movienight/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 /**
- * Created by Damjan on 26-May-18.
+ * Created by Damjan on 26-May-18. 
  */
 
 public class MainActivity extends AppCompatActivity{
